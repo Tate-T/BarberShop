293409cd70ee3d621710040eb57c31ee5e248e80
Maria2009gift <kovalenkom2009@gmail.com>
2024-08-27 18:44:53 +0300

    header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import './components/Header/Header.jsx'
+import { Header } from './components/Header/Header.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+
+
+export const Header = () => {
+    return (
+        <header>
+            <ul>
+                <a href="">About</a>
+                <a href="">Services and prices</a>
+                <a href="">Barbers</a>
+                <a href="">Contacts</a>
+            </ul>
+            <div>
+                <a href="tel:+380441111111">+38 044 111 11 11</a>
+                <button>ONLINE-BOOKING</button>
+            </div>
+        </header>
+    )
+}
