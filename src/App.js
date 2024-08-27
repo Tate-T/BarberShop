f52bf6569a92504b573e5c6890a5484bf0639cb4
Maria2009gift <kovalenkom2009@gmail.com>
2024-08-27 19:16:27 +0300

    About

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@
 import './App.css';
 import './components/Header/Header.jsx'
 import { Header } from './components/Header/Header.jsx';
+import {Hero} from './components/Hero/Hero.jsx'
+import { About } from './components/About/About.jsx'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <main>
+        <Hero/>
+        <About/>
+      </main>
     </div>
   );
 }
