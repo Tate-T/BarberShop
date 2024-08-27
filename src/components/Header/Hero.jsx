2c5c2bf4605a3a51babed922ec236abcd620084c
Daniil <d.kirykha2010@gmail.com>
2024-08-27 18:58:43 +0300

    hero

---
+++ src/components/Header/Hero.jsx
@@ -0,0 +1,15 @@
+export const Hero = () => {
+    return (
+        <section>
+            <h3>A hair salon for men in Kyiv</h3>
+            <h1>BarberShop</h1>
+            <p>We are experts in trendy men’s haircuts. We work quickly, carefully and with style.</p>
+            <button type="button">Back</button>
+            <button type="button">Forward</button>
+            <ul>
+                <li><a href="https://www.instagram.com/" target="_blank" rel="noreferrer noopener">Instagram</a></li>
+                <li><a href="https://m.youtube.com/" target="_blank" rel="noreferrer noopener">Youtube</a></li>
+            </ul>
+        </section>
+    )
+}
